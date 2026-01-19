e95610d22f49c0ea04e7978d95dbbb4738a40fa2
David-Chemaly <davidchemaly@hotmail.com>
2026-01-19 08:25:38 +0000

    working on population.py

--- population.py
+++ population.py
@@ -1,139 +1,99 @@
-import numpy as np
-import emcee
-import multiprocessing
-
+import os
+import corner
 import pickle
 import numpy as np
 from tqdm import tqdm
-
-import os 
-import pickle
-import scipy
-import corner 
-import matplotlib.pyplot as plt
-
 import multiprocessing as mp
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 18})
+from scipy.stats import norm
+
+import jax
+import jax.numpy as jnp
 
 import dynesty
 import dynesty.utils as dyut
 
-from orbit_fit import model_log_likelihood, orbit_prior_transform # import only for loading samples
+from utils import get_q
 
-def combine_data(path_save, N):
-    dict_flat, dict_params = {}, {}
-    all_true_params = []
-    max_length = 0
-    N_real = 0
-    for i in tqdm( range(N), leave=True):
+BAD_VAL = -1e15
 
-        try:
-            with open(f'{path_save}/xx_{i+1:03d}/dict_result.pkl', 'rb') as file:
-                dns = pickle.load(file)
+### Uniform ###
+def uniform(x, a, delta):
+    prob = np.where((x >= a-delta) & (x <= a+delta), 1./delta, 0.)
+    return prob
 
-            true_params = np.loadtxt(f'{path_save}/xx_{i+1:03d}/params_data.txt')
-            all_true_params.append(true_params[2])
+def prior_transform_uniform(p):
+    a0, delta0 = p
 
-            posterior_samples = dns['samps'][:,2] # CAREFUL HERE - 1 is the index of the parameter we are interested in
+    a1 = 2*a0
+    delta1 = 2*delta0
 
-            dict_params[i] = true_params
+    return [a1, delta1]
 
-            dict_flat[i] = posterior_samples
-
-            max_length = np.max([max_length, len(posterior_samples)])
-            N_real += 1
-        except:
-            print(f'Failed for i={i}')
-
-    samples_array = np.zeros([N_real, max_length])
-    for i in range(N_real):
-        try:
-            samples_array[i, :len(dict_flat[i])] = dict_flat[i]
-        except:
-            print(f'Failed for i={i}')
-    
-    with open(f'{path_save}/dict_q_N{N_real}.pkl', 'wb') as f:
-        pickle.dump(dict_flat, f)
-
-    return dict_flat, N_real
-
-def MCMC_fit(path_save, N, ndim=2, nwalkers=10, nsteps=1000):
-
-    dict_data, N_real = combine_data(path_save, N)
-
-    # Initial positions of the walkers
-    p0 = 2*np.random.uniform(size=(nwalkers, ndim))
-
-    # Set up the multiprocessing pool
-    multiprocessing.set_start_method("spawn", force=True)
-    with multiprocessing.Pool() as pool:
-        # Set up the MCMC sampler with the pool for parallel processing
-        sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior, args=(dict_data, ), pool=pool)
-        
-        # Run MCMC
-        sampler.run_mcmc(p0, nsteps, progress=True)
-
-    # Get the chain of samples after burn-in (e.g., first 1000 steps)
-    samples = sampler.get_chain(discard=200, thin=15, flat=True)
-
-    return samples, N_real
-
+### Gaussian ###
 def gaussian(x, mu, sigma):
-    return 1/np.sqrt(2*np.pi*sigma**2) * np.exp(-0.5*(x-mu)**2/sigma**2)
+    return 1/jnp.sqrt(2*jnp.pi*sigma**2) * jnp.exp(-0.5*(x-mu)**2/sigma**2)
 
-# Define the log-posterior function
-def log_posterior(theta, dict_data):
-    mu, sigma = theta
-    
-    # Prior: assuming broad uniform priors
-    if mu < 0:
-        return -np.inf
-    elif mu > 2:
-        return -np.inf
-    elif sigma < 0: 
-        return -np.inf
-    elif sigma > 2:
-        return -np.inf
-    
-    else:
-        # Log-prior (uniform priors, log of 1 is 0, so we can ignore it)
-        log_prior = 0
-        
-        log_likelihood = 0
-        for i in dict_data.keys():
-        # likelihood      = np.sum( gaussian(dict_data, mu, sigma) * np.int8(dict_data!=0), axis=1 ) / NN
-            likelihood       = np.mean( gaussian(dict_data[i], mu, sigma) )
-            log_likelihood  += np.log(likelihood)
-
-        return log_prior + log_likelihood
-
-def prior_transform(p):
-    #ndim = 12
+def prior_transform_gaussian(p):
     mu0, sigma0 = p
 
-    mu1 = 2*mu0
+    mu1    = 2*mu0
     sigma1 = 2*sigma0
 
     return [mu1, sigma1]
 
-def log_likelihood(theta, dict_data):
-    mu, sigma = theta
+### Binomial ###
+def binomial(x, mu1, mu2, sigma1, sigma2, prob=0.5):
+    return prob * (1/np.sqrt(2*np.pi*sigma1**2) * np.exp(-0.5*(x-mu1)**2/sigma1**2)) + \
+           (1-prob) * (1/np.sqrt(2*np.pi*sigma2**2) * np.exp(-0.5*(x-mu2)**2/sigma2**2))
+
+def prior_transform_binomial(p):
+    mu1, mu2, sigma1, sigma2, prob = p
+
+    mu1    = mu1
+    sigma1 = 2*sigma1
+    mu2    = mu2+1
+    sigma2 = 2*sigma2
+    prob   = prob
+
+    return [mu1, mu2, sigma1, sigma2, prob]
+
+### Likelihood ###
+def log_likelihood(theta, dict_data, pop_type='uniform'):
+    if pop_type == 'uniform':
+        pop_dist = uniform
+    elif pop_type == 'gaussian':
+        pop_dist = gaussian
+    elif pop_type == 'binomial':
+        pop_dist = binomial
 
     # Log-likelihood
     log_likelihood = 0
-    for i in dict_data.keys():
-        likelihood      = np.mean(gaussian(dict_data[i], mu, sigma))
+    for i in range(len(dict_data)):
+        likelihood      = np.mean(pop_dist(dict_data[i], *theta))
+        if likelihood <= 0:
+            return BAD_VAL
         log_likelihood += np.log(likelihood)
 
     return log_likelihood
 
-def dynesty_fit(dict_data, ndim=2, nlive=500):
+### Dynesty Fit ###
+def dynesty_fit(dict_data, ndim=2, nlive=500, pop_type='uniform'):
+    if pop_type == 'uniform':
+        prior_transform = prior_transform_uniform
+    elif pop_type == 'gaussian':
+        prior_transform = prior_transform_gaussian
+    elif pop_type == 'binomial':
+        prior_transform = prior_transform_binomial
+
     nthreads = os.cpu_count()
     mp.set_start_method("spawn", force=True)
     with mp.Pool(nthreads) as poo:
         dns = dynesty.DynamicNestedSampler(log_likelihood,
                                 prior_transform,
                                 ndim,
-                                logl_args=(dict_data, ),
+                                logl_args=(dict_data, pop_type),
                                 nlive=nlive,
                                 sample='unif',  
                                 pool=poo,
@@ -156,59 +116,57 @@ def dynesty_fit(dict_data, ndim=2, nlive=500):
 
     return dns_results
 
-if __name__ == '__main__':
-    # from jax_dynesty import *
-    q_true = 1.0
-    q_sig = 0.1
-    seed = 3
-    ndim = 14
-    nlive = 4000
-    sigma = 1
+if __name__ == "__main__":
+    fit_dist = 'gaussian'
+    ndim = 2
+    nlive = 500
 
-    PATH_SAVE = f'/data/dc824-2/S2S/q{q_true}_qsig{q_sig}_seed{seed}_ndim{ndim}_nlive{nlive}_sigma{sigma}'
+    sigma = 2
+    nlive = 2000
+    N_streams = 100
+    seeds = np.arange(N_streams)
+    path = '/data/dc824-2/MockStreams'
 
-    # i = 30
-    # n = i+1
-    # # MCMC fit
-    # samples, N_real = MCMC_fit(PATH_SAVE, n, ndim=2, nwalkers=10, nsteps=1000)
+    q_true, q_fits = [], []
+    for seed in seeds:
+        path_seed = os.path.join(path, f'seed{seed}')
+        if os.path.exists(os.path.join(path_seed,  f'dict_results_nlive{nlive}_sigma{sigma}.pkl')):
+            with open(os.path.join(path_seed, f'dict_results_nlive{nlive}_sigma{sigma}.pkl'), "rb") as f:
+                dict_results = pickle.load(f)
+            with open(os.path.join(path_seed, f'dict_stream.pkl'), "rb") as f:
+                dict_stream = pickle.load(f)
+            q_fits.append(get_q(*dict_results['samps'][:, 2:5].T))
+            q_true.append(dict_stream['params'][2])
+    q_true = np.array(q_true)
 
-    # for n in [25, 50, 100]:
+    q_true = q_true[arg_take]
+    new_q_fits = []
+    for arg in arg_take:
+        new_q_fits.append(q_fits[arg])
+    q_fits = new_q_fits
 
-    n = 75
-    # # Dynesty fit
-    dict_data, N_real = combine_data(PATH_SAVE, n)
-    dict_result = dynesty_fit(dict_data)
-    samples = dict_result['samps']
+    if fit_dist == 'uniform':
+        labels = [r'$a$', r'$b$']
+        print(f'Fitting {len(q_true)} using seed {sample_seed} streams with [{np.mean(q_true):.2f}, {abs(np.max(q_true)-np.min(q_true))/2:.2f}]')
+    elif fit_dist == 'gaussian':
+        labels = [r'$\mu$', r'$\sigma$']
+        print(f'Fitting {len(q_true)} using seed {sample_seed} streams with {np.mean(q_true):.2f} +/- {np.std(q_true):.2f}')
+    elif fit_dist == 'binomial':
+        labels = [r'$\mu_1$', r'$\mu_2$', r'$\sigma_1$', r'$\sigma_2$']
+        print(f'Fitting {len(q_true)} using seed {sample_seed} streams with {np.mean(q_true[q_true<1.0]):.2f} +/- {np.std(q_true[q_true<1.0]):.2f} and \
+                    {np.mean(q_true[q_true>=1.0]):.2f} +/- {np.std(q_true[q_true>=1.0]):.2f} instead of {true_a:.2f} +/- {true_b:.2f}')
 
-    np.save(PATH_SAVE+f'/population_samples_N{N_real}_dynesty.npy', samples)
+    dict_results = dynesty_fit(q_fits, ndim=ndim, nlive=nlive, pop_type=fit_dist)
+    with open(os.path.join(path, f'dict_pop_nlive{nlive}_sigma{sigma}_N{len(q_true)}_'+fit_type+f'_{true_a}-{true_b}_seed{sample_seed}.pkl'), 'wb') as f:
+        pickle.dump(dict_results, f)
 
-    fig = corner.corner(samples, 
+    # Plots the corner plots
+    figure = corner.corner(dict_results['samps'], 
+            labels=labels,
             color='blue',
-            quantiles=[0.16, 0.5, 0.84], 
+            quantiles=[0.16, 0.5, 0.84],
             show_titles=True, 
             title_kwargs={"fontsize": 16},
-            truths=[q_true, q_sig], 
-            truth_color='red',
-            labels=[r"q$_{mean}$", r"q$_{sigma}$"])
-    fig.savefig(f'{PATH_SAVE}/population_corner_N{N_real}_dynesty.pdf')
-
-    with open(f'{PATH_SAVE}/dict_q_N{N_real}.pkl', 'rb') as file:
-        dict_q= pickle.load(file)
-
-    average_q = 0
-    all_q = []
-    for i in dict_q.keys():
-        average_q += dict_q[i].mean()
-        all_q.extend(dict_q[i])
-    average_q /= len(dict_q)
-
-    plt.figure()
-    plt.hist(all_q, bins=30, color='blue', histtype='bar', ec='black')
-    plt.axvline(average_q, color='red', lw=2, label='Mean', linestyle='--')
-    plt.axvline(q_true, color='red', lw=2, label='True')
-    plt.xlabel('q')
-    plt.ylabel('Count')
-    plt.legend(loc='best')
-    plt.savefig(f'{PATH_SAVE}/q_hist_N{N_real}.pdf')
-
-    
+            truth_color='red')
+    figure.savefig(os.path.join(path, f'corner_plot_nlive{nlive}_sigma{sigma}_N{len(q_true)}.pdf'))
+    plt.close(figure)
