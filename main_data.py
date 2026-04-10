dd674b8b55e3d82f2a74c518eda6f851fccf72d1
David Chemaly <davidchemaly@hotmail.com>
2026-04-10 15:46:54 +0100

    new v_host with better uncertainty

--- main_data.py
+++ main_data.py
@@ -135,7 +135,7 @@ if __name__ == "__main__":
     prior_transform_fn = prior_transform
 
     PATH_DATA = f'/data/dc824-2/SGA_Streams'
-    PATH_OUT  = f'/data/dc824-2/SGA_Streams_Kinematics_SraightForward'
+    PATH_OUT  = f'/data/dc824-2/SGA_Streams_Kinematics'
     names = np.loadtxt(f'{PATH_DATA}/names.txt', dtype=str)
     STRRINGS_catalogue = pd.read_csv(f'{PATH_DATA}/STRRINGS_catalogue.csv')
     strings_df = pd.read_csv('STRRINGS.csv')
